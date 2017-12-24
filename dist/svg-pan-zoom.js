9f6307401d32b0f173fe32bc3374fca440d3d18f
baohongz <baohong.zhang@pfizer.com>
2017-12-24 10:02:08 -0500

    Modified svg-pan-zoom.js to show control at top-left

--- dist/svg-pan-zoom.js
+++ dist/svg-pan-zoom.js
@@ -38,14 +38,15 @@ module.exports = {
       var style = document.createElementNS(SvgUtils.svgNS, 'style')
       style.setAttribute('id', 'svg-pan-zoom-controls-styles')
       style.setAttribute('type', 'text/css')
-      style.textContent = '.svg-pan-zoom-control { cursor: pointer; fill: black; fill-opacity: 0.333; } .svg-pan-zoom-control:hover { fill-opacity: 0.8; } .svg-pan-zoom-control-background { fill: white; fill-opacity: 0.5; } .svg-pan-zoom-control-background { fill-opacity: 0.8; }'
+      style.textContent = '.svg-pan-zoom-control { cursor: pointer; fill: black; fill-opacity: 0.333; } .svg-pan-zoom-control:hover { fill-opacity: 0.8; } .svg-pan-zoom-control-background { fill: white; fill-opacity: 0.3; } .svg-pan-zoom-control-background { fill-opacity: 0.3; }'
       defs.appendChild(style)
     }
 
     // Zoom Group
+	// Baohong
     var zoomGroup = document.createElementNS(SvgUtils.svgNS, 'g');
     zoomGroup.setAttribute('id', 'svg-pan-zoom-controls');
-    zoomGroup.setAttribute('transform', 'translate(' + ( instance.width - 70 ) + ' ' + ( instance.height - 76 ) + ') scale(0.75)');
+    zoomGroup.setAttribute('transform', 'translate(0  0) scale(0.6)'); // Baohong: move to top-left 
     zoomGroup.setAttribute('class', 'svg-pan-zoom-control');
 
     // Control elements
